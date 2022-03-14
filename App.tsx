089f69666eb8b691476f951c2663389218f5a7e5
Gustavo Dolzan <gudolzan@gmail.com>
2022-03-13 22:58:31 -0300

    feat: implement react navigation

--- App.tsx
+++ App.tsx
@@ -1,76 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import {
-  ActivityIndicator,
-  FlatList,
-  Image,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-interface IProduct {
-  id: string;
-  title: string;
-  price: number;
-  thumbnail: string;
-}
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [isLoading, setLoading] = useState(true);
-  const [products, setProducts] = useState<IProduct[]>([]);
-
-  const getProducts = async () => {
-    try {
-      const query = 'maquininha';
-      const response = await fetch(
-        `https://api.mercadolibre.com/sites/MLB/search?q=${query}`
-      );
-      const { results } = await response.json();
-      setProducts(results.slice(0, 12));
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    getProducts();
-  }, []);
-
   return (
-    <View style={styles.container}>
-      {isLoading ? (
-        <ActivityIndicator />
-      ) : (
-        <FlatList
-          data={products}
-          keyExtractor={({ id }) => id}
-          renderItem={({ item }) => (
-            <View>
-              <Text>
-                {item.title}, {item.price}
-              </Text>
-              <Image style={styles.logo} source={{ uri: item.thumbnail }} />
-            </View>
-          )}
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Boas-vindas à Loja Online' }}
         />
-      )}
-      <StatusBar style="auto" />
-    </View>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo: {
-    height: 58,
-    width: 66,
-  },
-});
---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,76 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+interface IProduct {
+  id: string;
+  title: string;
+  price: number;
+  thumbnail: string;
+}
+
+export default function Component() {
+  const [isLoading, setLoading] = useState(true);
+  const [products, setProducts] = useState<IProduct[]>([]);
+
+  const getProducts = async () => {
+    try {
+      const query = 'maquininha';
+      const response = await fetch(
+        `https://api.mercadolibre.com/sites/MLB/search?q=${query}`
+      );
+      const { results } = await response.json();
+      setProducts(results.slice(0, 12));
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={products}
+          keyExtractor={({ id }) => id}
+          renderItem={({ item }) => (
+            <View>
+              <Text>
+                {item.title}, {item.price}
+              </Text>
+              <Image style={styles.logo} source={{ uri: item.thumbnail }} />
+            </View>
+          )}
+        />
+      )}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    height: 58,
+    width: 66,
+  },
+});
