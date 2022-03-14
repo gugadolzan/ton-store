6e34d8ab541807291c059738db965741ad98cd20
Gustavo Dolzan <gudolzan@gmail.com>
2022-03-13 21:06:33 -0300

    feat: fetch products from api

--- App.tsx
+++ App.tsx
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    try {
+      const query = 'maquininha';
+      const response = await fetch(
+        `https://api.mercadolibre.com/sites/MLB/search?q=${query}`
+      );
+      const { results } = await response.json();
+      setProducts(results.slice(0, 12));
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {isLoading ? <ActivityIndicator /> : <Text>Online Store</Text>}
       <StatusBar style="auto" />
     </View>
   );
