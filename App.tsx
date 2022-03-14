6f6534f3e06422c7baaea5f824c0ed46c9a290da
Gustavo Dolzan <gudolzan@gmail.com>
2022-03-13 23:00:31 -0300

    fix: isolate navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,7 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import HomeScreen from './screens/HomeScreen';
-
-const Stack = createNativeStackNavigator();
+import Navigation from './navigation';
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'Boas-vindas à Loja Online' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <Navigation />;
 }
