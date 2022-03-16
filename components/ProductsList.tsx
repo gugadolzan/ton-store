c745550a8a126fdc8075e8cbdc8998559616f28d
Gustavo Dolzan <gudolzan@gmail.com>
2022-03-16 16:47:34 -0300

    feat: implement add/remove item to/from cart button

--- components/ProductsList.tsx
+++ components/ProductsList.tsx
@@ -3,20 +3,11 @@ import { FlatList, Image, Pressable, Text, View } from 'react-native';
 
 import ShoppingCartContext from '../context/ShoppingCartContext';
 import styles from '../styles';
-import { IProduct, IProductsListProps } from '../types';
+import { IProductsListProps } from '../types';
 
 export default function ProductsList({ products }: IProductsListProps) {
-  const { shoppingCart, setShoppingCart } = useContext(ShoppingCartContext);
-
-  const addToCart = (product: IProduct) => {
-    setShoppingCart([...shoppingCart, product]);
-  };
-
-  const removeFromCart = (product: IProduct) => {
-    setShoppingCart(
-      shoppingCart.filter((item: IProduct) => item.id !== product.id)
-    );
-  };
+  const { addToCart, removeFromCart, shoppingCart } =
+    useContext(ShoppingCartContext);
 
   return (
     <FlatList
@@ -28,17 +19,32 @@ export default function ProductsList({ products }: IProductsListProps) {
             {item.title}, {item.price}
           </Text>
           <Image style={styles.logo} source={{ uri: item.thumbnail }} />
-          {shoppingCart.find((p) => p.id === item.id) ? (
-            <Pressable
-              style={styles.buttonRemove}
-              onPress={() => removeFromCart(item)}
-            >
-              <Text style={styles.buttonText}>Remover do carrinho</Text>
-            </Pressable>
-          ) : (
+
+          {!shoppingCart.find(({ product }) => product.id === item.id) ? (
             <Pressable style={styles.buttonAdd} onPress={() => addToCart(item)}>
               <Text style={styles.buttonText}>Adicionar ao carrinho</Text>
             </Pressable>
+          ) : (
+            <View>
+              <Pressable
+                style={styles.buttonAdd}
+                onPress={() => addToCart(item)}
+              >
+                <Text style={styles.buttonText}>+</Text>
+              </Pressable>
+              <Text>
+                {
+                  shoppingCart.find(({ product }) => product.id === item.id)
+                    ?.quantity
+                }
+              </Text>
+              <Pressable
+                style={styles.buttonRemove}
+                onPress={() => removeFromCart(item)}
+              >
+                <Text style={styles.buttonText}>-</Text>
+              </Pressable>
+            </View>
           )}
         </View>
       )}
--- screens/ShoppingCartScreen.tsx
+++ screens/ShoppingCartScreen.tsx
@@ -3,16 +3,9 @@ import { FlatList, Image, Pressable, Text, View } from 'react-native';
 
 import ShoppingCartContext from '../context/ShoppingCartContext';
 import styles from '../styles';
-import { IProduct } from '../types';
 
 export default function ShoppingCartScreen() {
-  const { shoppingCart, setShoppingCart } = useContext(ShoppingCartContext);
-
-  const removeFromCart = (product: IProduct) => {
-    setShoppingCart(
-      shoppingCart.filter((item: IProduct) => item.id !== product.id)
-    );
-  };
+  const { addToCart, removeFromCart, shoppingCart } = useContext(ShoppingCartContext);
 
   if (shoppingCart.length === 0) {
     return (
@@ -27,19 +20,33 @@ export default function ShoppingCartScreen() {
       <Text>{shoppingCart.length} produtos adicionados:</Text>
       <FlatList
         data={shoppingCart}
-        keyExtractor={({ id }) => id}
+        keyExtractor={({ product: { id } }) => id}
         renderItem={({ item }) => (
           <View style={styles.product}>
             <Text>
-              {item.title}, {item.price}
+              {item.product.title}, {item.product.price}
             </Text>
-            <Image style={styles.logo} source={{ uri: item.thumbnail }} />
-            <Pressable
-              style={styles.buttonRemove}
-              onPress={() => removeFromCart(item)}
-            >
-              <Text style={styles.buttonText}>Remover do carrinho</Text>
-            </Pressable>
+            <Image style={styles.logo} source={{ uri: item.product.thumbnail }} />
+            <View>
+              <Pressable
+                style={styles.buttonAdd}
+                onPress={() => addToCart(item.product)}
+              >
+                <Text style={styles.buttonText}>+</Text>
+              </Pressable>
+              <Text>
+                {
+                  shoppingCart.find(({ product }) => product.id === item.product.id)
+                    ?.quantity
+                }
+              </Text>
+              <Pressable
+                style={styles.buttonRemove}
+                onPress={() => removeFromCart(item.product)}
+              >
+                <Text style={styles.buttonText}>-</Text>
+              </Pressable>
+            </View>
           </View>
         )}
       />
