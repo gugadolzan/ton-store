643460a0f5e6e855ba917ad16d4a11cf4c942cb6
Gustavo Dolzan <gudolzan@gmail.com>
2022-03-16 14:34:40 -0300

    feat: render products in shopping cart

--- screens/ShoppingCartScreen.tsx
+++ screens/ShoppingCartScreen.tsx
@@ -1,10 +1,48 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useContext } from 'react';
+import { FlatList, Image, Pressable, Text, View } from 'react-native';
+
+import styles from '../styles';
+import ShoppingCartContext from '../context/ShoppingCartContext';
+import { IProduct } from '../types';
 
 export default function ShoppingCartScreen() {
+  const { shoppingCart, setShoppingCart } = useContext(ShoppingCartContext);
+
+  const removeFromCart = (product: IProduct) => {
+    setShoppingCart(
+      shoppingCart.filter((item: IProduct) => item.id !== product.id)
+    );
+  };
+
+  if (shoppingCart.length === 0) {
+    return (
+      <View style={styles.container}>
+        <Text>Nenhum produto no carrinho!</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>ShoppingCartScreen</Text>
+    <View>
+      <Text>{shoppingCart.length} produtos adicionados:</Text>
+      <FlatList
+        data={shoppingCart}
+        keyExtractor={({ id }) => id}
+        renderItem={({ item }) => (
+          <View style={styles.product}>
+            <Text>
+              {item.title}, {item.price}
+            </Text>
+            <Image style={styles.logo} source={{ uri: item.thumbnail }} />
+            <Pressable
+              style={styles.buttonRemove}
+              onPress={() => removeFromCart(item)}
+            >
+              <Text style={styles.buttonText}>Remover do carrinho</Text>
+            </Pressable>
+          </View>
+        )}
+      />
     </View>
   );
 }
