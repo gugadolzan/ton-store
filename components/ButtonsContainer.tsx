8bcbc93f77121bd30ad75666d4bb2f23c21e7825
Gustavo Dolzan <gudolzan@gmail.com>
2022-03-16 21:58:15 -0300

    refactor: componetize buttons container

---
+++ components/ButtonsContainer.tsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import { Pressable, Text, View } from 'react-native';
+import ShoppingCartContext from '../context/ShoppingCartContext';
+
+import styles from '../styles';
+import { IProductItemProps } from '../types';
+
+export default function ButtonsContainer({ product }: IProductItemProps) {
+  const { addToCart, removeFromCart, shoppingCart } =
+    useContext(ShoppingCartContext);
+
+  return (
+    <View style={styles.buttonsContainer}>
+      <Pressable style={styles.buttonPlus} onPress={() => addToCart(product)}>
+        <Text style={styles.buttonSign}>+</Text>
+      </Pressable>
+      <Text style={styles.quantity}>
+        {shoppingCart.find((item) => item.product.id === product.id)?.quantity}
+      </Text>
+      <Pressable
+        style={styles.buttonMinus}
+        onPress={() => removeFromCart(product)}
+      >
+        <Text style={styles.buttonSign}>-</Text>
+      </Pressable>
+    </View>
+  );
+}
--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -4,10 +4,10 @@ import { Image, Pressable, Text, View } from 'react-native';
 import ShoppingCartContext from '../context/ShoppingCartContext';
 import styles from '../styles';
 import { IProductItemProps } from '../types';
+import ButtonsContainer from './ButtonsContainer';
 
 export default function ProductItem({ product }: IProductItemProps) {
-  const { addToCart, removeFromCart, shoppingCart } =
-    useContext(ShoppingCartContext);
+  const { addToCart, shoppingCart } = useContext(ShoppingCartContext);
 
   return (
     <View style={styles.productContainer}>
@@ -31,26 +31,7 @@ export default function ProductItem({ product }: IProductItemProps) {
           <Text style={styles.buttonText}>Adicionar ao carrinho</Text>
         </Pressable>
       ) : (
-        <View style={styles.buttonsContainer}>
-          <Pressable
-            style={styles.buttonPlus}
-            onPress={() => addToCart(product)}
-          >
-            <Text style={styles.buttonSign}>+</Text>
-          </Pressable>
-          <Text style={styles.quantity}>
-            {
-              shoppingCart.find((item) => item.product.id === product.id)
-                ?.quantity
-            }
-          </Text>
-          <Pressable
-            style={styles.buttonMinus}
-            onPress={() => removeFromCart(product)}
-          >
-            <Text style={styles.buttonSign}>-</Text>
-          </Pressable>
-        </View>
+        <ButtonsContainer product={product} />
       )}
     </View>
   );
