c74586cf917460f22eded3c10639a35e50178bc1
Gustavo Dolzan <gudolzan@gmail.com>
2022-03-16 22:16:41 -0300

    refactor: update shopping cart provider

--- context/ShoppingCartProvider.tsx
+++ context/ShoppingCartProvider.tsx
@@ -3,19 +3,28 @@ import React, { FC, useState } from 'react';
 import ShoppingCartContext from './ShoppingCartContext';
 import { IProduct, IShoppingCartItem } from '../types';
 
+/**
+ * ShoppingCartProvider is a component that provides the shopping cart context to its children
+ * and acts as a global state.
+ */
 const ShoppingCartProvider: FC = ({ children }) => {
   const [shoppingCart, setShoppingCart] = useState<IShoppingCartItem[]>([]);
 
+  /**
+   * Adds a product to the shopping cart
+   */
   const addToCart = (product: IProduct) => {
     const itemExists = shoppingCart.some(
       (item) => item.product.id === product.id
     );
 
+    // If the item does not exist, add it to the cart
     if (!itemExists) {
       setShoppingCart([...shoppingCart, { product, quantity: 1 }]);
       return;
     }
 
+    // If the item exists, increment the quantity
     const newShoppingCart = shoppingCart.map((item) =>
       item.product.id === product.id
         ? { product, quantity: item.quantity + 1 }
@@ -25,6 +34,9 @@ const ShoppingCartProvider: FC = ({ children }) => {
     setShoppingCart(newShoppingCart);
   };
 
+  /**
+   * Removes a product from the shopping cart
+   */
   const removeFromCart = (product: IProduct) => {
     let newShoppingCart = shoppingCart.map((item) =>
       item.product.id === product.id
@@ -32,10 +44,11 @@ const ShoppingCartProvider: FC = ({ children }) => {
         : item
     );
 
-    const isZero = newShoppingCart.find(
+    const isZero = newShoppingCart.some(
       (item) => item.product.id === product.id && item.quantity === 0
     );
 
+    // If the quantity is 0, remove the item from the cart
     if (isZero) {
       newShoppingCart = shoppingCart.filter(
         (item) => item.product.id !== product.id
@@ -46,9 +59,7 @@ const ShoppingCartProvider: FC = ({ children }) => {
   };
 
   return (
-    <ShoppingCartContext.Provider
-      value={{ addToCart, removeFromCart, shoppingCart }}
-    >
+    <ShoppingCartContext.Provider value={{ addToCart, removeFromCart, shoppingCart }}>
       {children}
     </ShoppingCartContext.Provider>
   );
